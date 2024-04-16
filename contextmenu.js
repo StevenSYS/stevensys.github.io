1a0da1f371511154e4e3071cd0501d6c5269b0f5
StevenSYS <stevensys510@gmail.com>
2024-04-16 12:26:24 +0000

    Changing stuff.

--- contextmenu.js
+++ contextmenu.js
@@ -1,5 +1,5 @@
 var cmClosed = 1;
-if (localStorage.getItem("darkmode") == "enabled") {$("body").addClass("dark");} else {$("body").removeClass("dark");};
+if (localStorage.getItem("darkmode") == "enabled") {$("body").addClass("dark");} else {$("body").removeClass("dark");}
 
 addEventListener("contextmenu", (event) => {
   rcSrc = event.target.getAttribute("src");
@@ -8,33 +8,31 @@ addEventListener("contextmenu", (event) => {
   event.preventDefault();
   $(".contextmenu").css("display", "block");
   if ($("body").hasClass("dark")) {$(".darkmode").html("Light Mode");} else {$(".darkmode").html("Dark Mode");}
-  if(selectionType.type == "Range") {$(".text").css("display", "block");} else {$(".text").css("display", "none");};
-  if(rcHref != null) {$(".ctext").css("display", "block");} else {$(".ctext").css("display", "none");};
-  if(event.target.nodeName == "IMG") {$(".image").css("display", "block");} else {$(".image").css("display", "none");};
-  if(new Date().getMonth() >= 10) {$(".christmas").css("display", "block");} else {$(".christmas").css("display", "none");};
-  if(new Date().getMonth() == 9) {$(".halloween").css("display", "block");} else {$(".halloween").css("display", "none");};
+  if (selectionType.type == "Range") {$(".text").css("display", "block");} else {$(".text").css("display", "none");}
+  if (rcHref != null) {$(".ctext").css("display", "block");} else {$(".ctext").css("display", "none");}
+  if (event.target.nodeName == "IMG") {$(".image").css("display", "block");} else {$(".image").css("display", "none");}
+  if (new Date().getMonth() >= 10) {$(".christmas").css("display", "block");} else {$(".christmas").css("display", "none");}
+  if (new Date().getMonth() == 9) {$(".halloween").css("display", "block");} else {$(".halloween").css("display", "none");}
   $(".contextmenu").css("left", event.clientX+"px");
   $(".contextmenu").css("top", event.clientY+"px");
   $(".contextmenu").effect("slide", 200, cmClosed = 0);
-});
+})
 
 addEventListener("mousedown", (event) => {
   if (event.button == 0 && $(".contextmenu").css("display") != "none" && cmClosed == 0) {
       cmClosed = 1;
       $(".contextmenu").effect("drop", 250, $(".contextmenu").css("display", "none"));
-}});
+}})
 
 function moveable(object) {
-  // Terrible Code! Don't look!
   $(object).css("position", "absolute");
   $(object).draggable();
   $(object).draggable("destroy");
   $(object).draggable();
-  // You can look now.
   $(object).css("display", "block");
   $(object).css("left", $(".contextmenu").css("left"));
   $(object).css("top", $(".contextmenu").css("top"));
-};
+}
 
 function darkmode() {
   if ($("body").hasClass("dark")) {
@@ -43,4 +41,4 @@ function darkmode() {
   } else {
     $("body").addClass("dark");
     localStorage.setItem("darkmode","enabled")
-}};
+}}
