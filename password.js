0b089b88e5dd926f014a51682fc30a36b4b20a2f
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2024-09-04 16:43:52 +0000

    Fix license thing.

--- password.js
+++ password.js
@@ -4,12 +4,12 @@
 	The StevenSYS Homepage is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Lesser General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+	any later version.
 
-    The StevenSYS Homepage is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+	The StevenSYS Homepage is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
 
 	You should have received a copy of the GNU Lesser General Public License along with The StevenSYS Homepage.
 	If not, see <https://www.gnu.org/licenses/>.
