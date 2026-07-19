a06829961ba851e3438bfd320a8baa238ae57235
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2026-07-19 16:33:42 +0000

    Changed the release names to the music names instead of the video name.
    I would like to rewrite the PHP files in C.

--- i6340/generate.sh
+++ i6340/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 php homePage.php > index.html
 php releasePage.php 0 > thunderdream.html
-php releasePage.php 1 > dancingCube.html
-php releasePage.php 2 > isfish.html
-php releasePage.php 3 > is3dfish.html
-php releasePage.php 4 > theBoss.html
-php releasePage.php 5 > ismusicfish.html
+php releasePage.php 1 > cubeGroove.html
+php releasePage.php 2 > ismusic.html
+php releasePage.php 3 > is3dmusic.html
+php releasePage.php 4 > spinningRing.html
+php releasePage.php 5 > isremixedmusic.html
